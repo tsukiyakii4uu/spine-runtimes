8767b81e177336b6f5f700ae7e870f110fdc0818
Davide Tantillo <iamdjj@gmail.com>
2025-11-05 11:57:56 +0100

    Made OnAnimationEvent more generic allowing empty params to be used as any.

--- spine-ts/spine-construct3/src/c3runtime/conditions.ts
+++ spine-ts/spine-construct3/src/c3runtime/conditions.ts
@@ -9,9 +9,10 @@ C3.Plugins.EsotericSoftware_SpineConstruct3.Cnds =
 		return true;
 	},
 	OnAnimationEvent (this: SpineC3Instance, event: string, track: number, animation: string) {
-		return this.triggeredEventName === event
-			&& this.triggeredEventTrack === track
-			&& this.triggeredEventAnimation === animation;
+		const eventMatches = event === "" || this.triggeredEventName === event;
+		const trackMatches = track === -1 || this.triggeredEventTrack === track;
+		const animationMatches = animation === "" || this.triggeredEventAnimation === animation;
+		return eventMatches && trackMatches && animationMatches;
 	},
 	IsFlippedX (this: SpineC3Instance) {
 		return this.propFlipX;
