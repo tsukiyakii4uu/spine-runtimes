ba02dfa4832293e2e382cacc790fb839fcfc0f59
Davide Tantillo <iamdjj@gmail.com>
2026-02-20 12:40:52 +0100

    [ts][pixi-v7] Fix setup pose flash on first frame. See #3029.

--- spine-ts/spine-pixi-v7/src/Spine.ts
+++ spine-ts/spine-pixi-v7/src/Spine.ts
@@ -338,7 +338,9 @@ export class Spine extends Container {
 			this.interactiveChildren = true;
 			this.hitArea = null;
 		}
-		this.calculateBounds();
+		if (!this.hasNeverUpdated) {
+			this.calculateBounds();
+		}
 	}
 	private _boundsPoint = new Point();
 	private _boundsSpineID = -1;
